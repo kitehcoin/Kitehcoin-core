a08e602177a045dba7e858c687f8172e2787fdad
kitehcoin <jesentor86@outlook.com>
2025-06-03 20:11:12 +0200

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_71_0
-$(package)_download_path=https://archives.boost.io/release/1.71.0/source/
+$(package)_version=1_81_0
+$(package)_download_path=https://archives.boost.io/release/1.81.0/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
+$(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
 
 # incase need to lower boost version for older os
 # $(package)_version=1_63_0
